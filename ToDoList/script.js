8677ac374cb844254824a9adbc7c6b6c8f3c1a77
Garv <garvmodi269@gmail.com>
2024-07-03 22:34:48 +0530

    Updated Layout

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -1,21 +1,31 @@
 let input=document.getElementById("TaskInput");
 let button=document.querySelector("button");
 let list=document.getElementById("list");
-let div=document.getElementsByClassName("listContainer");
+let checkboxDiv=document.getElementsByClassName('checkboxDiv')[0];//class returns a collection
 
 button.addEventListener("click",(e) =>{
     if(input.value != ""){
         let listItem=document.createElement("li");
-        let completeBtn=document.createElement("button");
-        completeBtn.textContent="Done";
-        completeBtn.style.margin="0px 10px";
+        let checkIteam=document.createElement("input");
+        
+        checkIteam.type="checkbox";
+        checkIteam.style.width="20px";
+        checkIteam.style.height="20px";
+        checkIteam.style.marginUp="30px";
+
         listItem.textContent=input.value;
+        
         list.appendChild(listItem);
-        listItem.appendChild(completeBtn);
+        checkboxDiv.appendChild(checkIteam);
         input.value="";//clear the inputBox
 
-        completeBtn.addEventListener("click" , (e)=>{
-            listItem.style.textDecorationLine="line-through";
+        checkIteam.addEventListener("change",(e)=>{
+            if(checkIteam.checked){
+                listItem.style.textDecorationLine="line-through";
+            }
+            else{
+            listItem.style.textDecorationLine="none";
+            }
         })
     }
 })
